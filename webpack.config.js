cadb08a88f314cce68d14df5f689973eed9d8add
codbear <douglas.meunier@openclassrooms.com>
2021-03-25 18:39:03 +0100

    fix(webpack): add support for svg files

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ const config = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
             loader: 'url-loader',
