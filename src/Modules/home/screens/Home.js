66bbd1d404632efa929a958209740044958bc2b9
codbear <douglas.meunier@openclassrooms.com>
2021-04-09 08:42:18 +0200

    feat(home): add dropdown to refine research

--- src/Modules/home/screens/Home.js
+++ src/Modules/home/screens/Home.js
@@ -1,11 +1,18 @@
 import Header from '../../header';
-import SearchBar from '../../search';
+import { SearchBar, TagSelect } from '../../search';
 import KitchenRecipesCollection from '../../kitchenRecipes';
+// eslint-disable-next-line no-unused-vars
+import styles from './Home.scss';
 
 const Home = () => (`
     <div class='container'>
       ${Header()}
       ${SearchBar()}
+      <div class='search-selector-group'>
+        ${TagSelect('ingredients', ['tag1', 'tag2', 'tag3'], 'primary')}
+        ${TagSelect('appliance', ['tag1', 'tag2', 'tag3'], 'success')}
+        ${TagSelect('ustensils', ['tag1', 'tag2', 'tag3'], 'danger')}
+      </div>
       ${KitchenRecipesCollection()}
     </div>`
 );
