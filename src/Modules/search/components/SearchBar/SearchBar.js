def7eb25361f861b045a9f8cef8d142e4c280238
codbear <douglas.meunier@openclassrooms.com>
2021-04-09 08:41:56 +0200

    feat(search): create tag component

--- src/Modules/search/components/SearchBar/SearchBar.js
+++ src/Modules/search/components/SearchBar/SearchBar.js
@@ -3,16 +3,27 @@ import styles from './SearchBar.scss';
 
 const SearchBar = () => {
   const label = 'Rechercher un ingrédient, appareil, ustensile ou une recette';
+  const value = '';
+
+  const handleChange = (event) => {
+    value = event.target.value;
+  };
+
+  window.setTimeout(() => {
+    const $input = document.getElementById('search-bar');
+    console.log($input);
+  });
 
   return (`
-    <div>
+    <form>
       <input 
+        id="search-bar"
         class="form-control search-bar" 
         type="text"
         placeholder="${label}"
         aria-label="${label}"
       />
-    </div>`
+    </form>`
   );
 };
 
