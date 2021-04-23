c716adcfbcf0f385fc904c801f621283b55c778d
codbear <douglas.meunier@openclassrooms.com>
2021-04-23 12:37:58 +0200

    feat(app): render all recipes in initial app state

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './Modules/kitchenRecipes';
 import './Modules/search';
+import recipeDataToRecipeCard from './Modules/kitchenRecipes/services';
+import recipes from './Modules/kitchenRecipes/assets/data/recipes.json';
+
+const recipeCards = recipeDataToRecipeCard(recipes);
+
+const recipesCollectionFragment = document.createDocumentFragment();
+
+recipeCards.forEach((recipeCard) => {
+  recipesCollectionFragment.appendChild(recipeCard);
+});
+
+const recipesCollectionElement = document.getElementById('kitchenRecipesCollection');
+recipesCollectionElement.appendChild(recipesCollectionFragment);
