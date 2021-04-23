6fcce7f48dd158db2a8f4f0e8acd461451692521
codbear <douglas.meunier@openclassrooms.com>
2021-04-23 12:34:35 +0200

    chore(translations): remove translations package

--- src/Modules/translations/services/translate.js
+++
@@ -1,7 +0,0 @@
-import fr from '../locales/fr.json';
-
-const translations = { fr };
-
-const translate = (string, locale = 'fr') => translations[locale][string] ?? string;
-
-export default translate;
