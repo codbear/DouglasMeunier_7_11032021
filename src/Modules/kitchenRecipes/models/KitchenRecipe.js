07c6cd7b85fc876a9bfd5c7b497f7fb34742bb45
codbear <douglas.meunier@openclassrooms.com>
2021-04-24 10:50:19 +0200

    feat(kitchen-recipe): create a model for kitchen recipe

---
+++ src/Modules/kitchenRecipes/models/KitchenRecipe.js
@@ -0,0 +1,21 @@
+import kitchenRecipeCardFactory from '../factories';
+
+export default class KitchenRecipe {
+  constructor(recipeData) {
+    this.hydrate(recipeData);
+    this.fragment = kitchenRecipeCardFactory(this);
+  }
+
+  hydrate(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.servings = data.servings;
+    this.ingredients = data.ingredients;
+    this.duration = data.time;
+    this.description = data.description;
+    this.appliance = data.appliance;
+    this.utensils = data.ustensils;
+
+    return this;
+  }
+}
