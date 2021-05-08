1450ba561a80e557036c5b1e94b5fc41c66fc1d3
codbear <douglas.meunier@openclassrooms.com>
2021-05-08 18:40:26 +0200

    refactor(kitchen-recipes): add a method to get a clone of the kitchenRecipeCard

--- src/Modules/kitchenRecipes/models/KitchenRecipe.js
+++ src/Modules/kitchenRecipes/models/KitchenRecipe.js
@@ -1,6 +1,10 @@
 import { kitchenRecipeCardFactory } from '../factories';
 
 export default class KitchenRecipe {
+  /**
+   *
+   * @param {Object.<string, *>} recipeData
+   */
   constructor(recipeData) {
     this.id = recipeData.id;
     this.name = recipeData.name;
@@ -10,6 +14,14 @@ export default class KitchenRecipe {
     this.description = recipeData.description;
     this.appliance = recipeData.appliance;
     this.utensils = recipeData.ustensils;
-    this.element = kitchenRecipeCardFactory(this);
+    this.node = kitchenRecipeCardFactory(this);
+  }
+
+  /**
+   *
+   * @return {Node}
+   */
+  getNode() {
+    return this.node.cloneNode(true);
   }
 }
